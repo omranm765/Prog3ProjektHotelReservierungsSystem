74b4c812dfaae5bcdcbe59d621ea8f4373e92d23
omranm765 <126417794+omranm765@users.noreply.github.com>
2024-03-25 23:53:23 +0100

    Test

--- Prog3ProjektHotelReservierungsSystem/src/main/java/com/example/prog3projekthotelreservierungssystem/Gast.java
+++ Prog3ProjektHotelReservierungsSystem/src/main/java/com/example/prog3projekthotelreservierungssystem/Gast.java
@@ -11,7 +11,7 @@ public class Gast extends Person{
         super(vorname, name, email, geburtsdatum, telefonNr);
     }
 
-
+    //HALLO
     @Override
     public String toString() {
         return super.toString() + "\nGastID: ";
